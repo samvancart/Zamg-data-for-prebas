0ce9b139c54dd173cff87a175cec37dc69d9feba
samvancart <sam02cart@gmail.com>
2022-12-22 11:08:33 +0200

    build api call

--- get_data.py
+++ get_data.py
@@ -10,34 +10,68 @@ from datetime import timedelta
 # "https://dataset.api.hub.zamg.ac.at/v1/timeseries/historical/spartacus-v1-1d-1km?parameters=RR&parameters=Tn&parameters=Tx&start=1961-01-01T00%3A00%3A00.000Z&end=2022-12-19T00%3A00%3A00.000Z&lat_lon=48.032695%2C+14.966223&output_format=csv&filename=SPARTACUS+-+Spatial+Reanalysis+Dataset+for+Climate+in+Austria+Datensatz_19610101_20221219"
 
 
+def_url = "https://dataset.api.hub.zamg.ac.at"
+def_version = "v1"
+def_type = "timeseries"
+def_mode = "historical"
+def_id = "spartacus-v1-1d-1km?"
+def_parameters = ["RR", "Tn", "Tx"]
+def_start_date = "1961-01-01"
+def_start_time = "T00:00"
+def_end_date = str(date.today()-timedelta(days=2))
+def_end_time = "T00:00"
+def_coordinates = [["48.032695", "14.966223"]]
+def_format = "csv"
+
+
 # def get_zamg(dataset, parameters, start, end, coordinates):
 
 
 
 
-def build_api_call(base_url, dataset, parameters, start, end, coordinates):
+def build_api_call(
+    base_url = def_url,
+    version = def_version,
+    type = def_type,
+    mode = def_mode,
+    id = def_id, 
+    parameters=def_parameters, 
+    start_date=def_start_date, 
+    start_time=def_start_time,
+    end_date=def_end_date,
+    end_time=def_end_time, 
+    coordinates=def_coordinates,
+    format=def_format
+    ):
     """ Builds call to zamg api
 
         Parameters: 
-            base_url (string): url of api 
-            dataset (string): Name of dataset
-            parameters (list string): The values to include
-            start (string): start date and time
-            end (string): end date and time
+            base_url (string): url of zamg api
+            version (string): api version
+            type (string): type of data (station, grid, timeseries)
+            mode (string): mode of data (historical, current, forecast)
+            id (string): Name of dataset
+            parameters (list string): The values to include in the query
+            start_date (string): start date
+            start_time (string): start time
+            end_date (string): end date
+            end_time (string): end time
             coordinates (list list string): list of coordinates as [['lat','lon'],['lat','lon']]
+            format (string): format of returned data (csv, geojson, netcdf)
         Returns:
             built call (string): Call to the api with requested parameters 
     
     """
-    url = base_url + dataset
+    
+    url = "/".join([base_url, version, type, mode, id])
     params ="parameters=" + ",".join(parameters)
     coords = list(map(",".join,coordinates))
     lat_lon = list(map(lambda old_string: "lat_lon=" + old_string, coords))
     lat_lons = "&".join(lat_lon)
-    format = "output_format=csv"
-    start = "start=" + start
-    end = "end=" + end
-    api_call_list = [url, params, start, end, lat_lons, format]
+    format_type = "output_format=" + format
+    start = "start=" + start_date + start_time
+    end = "end=" + end_date + end_time
+    api_call_list = [url, params, start, end, lat_lons, format_type]
     api_call = "&".join(api_call_list)
     return api_call
 
@@ -62,4 +96,4 @@ coordinates = [
     ]
 ]
 
-print(build_api_call(url,dataset, spartacus_params, start, end, coordinates))
+print(build_api_call(coordinates=coordinates))
