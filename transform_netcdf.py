1340beef9e1cc29b3b6acc2223c4782b6e21f60f
samvancart <sam02cart@gmail.com>
2023-03-08 15:13:50 +0200

    preprocess netCDF and write csv from processed df

--- transform_netcdf.py
+++ transform_netcdf.py
@@ -2,56 +2,47 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 from netCDF4 import Dataset
+from functools import partial
 
-path = "data/net_cdf/t2m/2015/INCAL_HOURLY_T2M_201501.nc"
-data = xr.open_dataset(path)
+
+def _preprocess(x, lon_bnds, lat_bnds):
+    # Remove unwanted data by coordinates
+    ds = x.where((x.lat<lat_bnds[1]) & (x.lat>=lat_bnds[0]) & (x.lon<lon_bnds[1]) & (x.lon>=lon_bnds[0]), drop=True)
+    # Return daily means
+    return ds.resample(time="1d").mean()
+
+path = "data/net_cdf/t2m/2015/INCAL_HOURLY_T2M"
+# path = "data/net_cdf/t2m/2015/INCAL_DAILY_STYRIA_T2M_2015.nc"
+# data = xr.open_dataset(path)
 # data = Dataset(path, 'r')
 
-# print(data)
-data=data.resample(time="1d").mean()
-print(data)
+# BOX BOUNDARIES
+lon_bnds, lat_bnds = (13, 16.7), (46, 47.4)
+partial_func = partial(_preprocess, lon_bnds=lon_bnds, lat_bnds=lat_bnds)
 
-lat = data.variables['lat']
-lon = data.variables['lon']
-time = data.variables['time']
+# OPEN ALL DATASETS AT ONCE
+data = xr.open_mfdataset(
+    f"{path}_*.nc", combine='nested', concat_dim='time', preprocess=partial_func
+    # f"{path}_201501.nc", combine='nested', concat_dim='time', preprocess=partial_func
+)
 
-t2m = data.variables['T2M'] 
-
-# print(lat[250])
-# print(np.shape(t2m.values[0][0]))
-# print(np.shape(time))
-# print(data.coords)
-test_lat=lat.values
-test_lon=lon.values
-test_t2m=t2m.values
-test_time = time.values
-# print(time)
-# print(np.shape(test_t2m))
-# print(test_time)
-
-
-
-
-
-
-# print(np.shape(test_t2m))
-# d = {'time':test_time, 'T2M':test_t2m, 'lat':lat, 'lon':lon}
-# d = {'T2M':test_t2m}
-# df = pd.DataFrame(data=d)
+# CONVERT TO DF
 df = data.to_dataframe()
-# lat_bounds=df.loc[(df['lat'] >= 46) & (df['lat'] <= 47.4)]
-# lon_bounds=df.loc[(df['lon'] >= 13) & (df['lon'] <= 16.7)]
-df=df.loc[(df['lat'] >= 46) & (df['lat'] <= 47.4) & (df['lon'] >= 13) & (df['lon'] <= 16.7)]
 df=df[['T2M','lat','lon']]
 new_index=df.index.droplevel(['x', 'y'])
 df.set_index(new_index, inplace=True)
-# print(df.index)
-# print(df.columns)
-# df = pd.merge(lat_bounds,lon_bounds)
-file = 'data/INCAL_DAILY_T2M_201501.styria.csv'
+
+# DROP NAN VALUES
+df.dropna(inplace=True)
+
+# GROUP BY COORDINATES
+grouped = df.groupby(["lat", "lon"],as_index=False)
+df = pd.concat([g for _, g in grouped])
+
+# WRITE TO CSV
+file = 'data/INCAL_DAILY_T2M_2015_styria_grouped.csv'
 df.to_csv(file)
 print(df)
-
 
 
 
